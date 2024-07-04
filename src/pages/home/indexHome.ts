801f49975991d40c94db259f947fb4c2a5b909c7
krzysztofee <gitaragra@gmail.com>
2024-07-04 15:16:24 +0200

    Add class Scroll

--- src/pages/home/indexHome.ts
+++ src/pages/home/indexHome.ts
@@ -1,1 +1,22 @@
-console.log('eeee',)
+class Scroll {
+  #btnHero = document.getElementById("hero");
+  #btnComposition = document.getElementById("composition");
+  #btnProducts = document.getElementById("products");
+  #sectionComposition = document.querySelector("[data-composition]");
+
+  constructor() {
+    this.#scrollEvent();
+  }
+  #handleScroll() {
+    this.#sectionComposition?.scrollIntoView({ behavior: "smooth" });
+  }
+
+  #scrollEvent() {
+    this.#btnComposition?.addEventListener(
+      "click",
+      this.#handleScroll.bind(this)
+    );
+  }
+}
+
+new Scroll();
