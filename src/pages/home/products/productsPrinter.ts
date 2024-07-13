c2935a4977de4197191b91f1701ce38458f0c45a
krzysztofee <gitaragra@gmail.com>
2024-07-13 17:10:11 +0200

    Add drops around pictures

--- src/pages/home/products/productsPrinter.ts
+++ src/pages/home/products/productsPrinter.ts
@@ -29,7 +29,6 @@ export class ProductsPrinter {
     });
   }
   #createProductsContainer() {
-
     this.#productsContainerEl.classList.add(
       "even-columns-4",
       "relative",
@@ -44,18 +43,17 @@ export class ProductsPrinter {
       ".products-container"
     );
 
-        const lastProductsContainerX =
-          productsContainersElems[productsContainersElems.length - 1];
+    const lastProductsContainerX =
+      productsContainersElems[productsContainersElems.length - 1];
 
     const productsElems =
       lastProductsContainerX.querySelectorAll(".product-container");
-   
 
-      const allProducts = document.querySelectorAll(".product-container");
+    const allProducts = document.querySelectorAll(".product-container");
 
     console.log("", productsContainersElems);
 
-    if (productsElems?.length % 4 !== 0 && productsContainersElems.length>2) {
+    if (productsElems?.length % 4 !== 0 && productsContainersElems.length > 2) {
       const restNumber = productsElems?.length % 4;
       console.log("ee", restNumber);
 
@@ -68,32 +66,94 @@ export class ProductsPrinter {
         lastProductsContainer.querySelectorAll(".product-container");
       const productsToRemove = Array.from(products).slice(0, restNumber);
 
-      console.log("", prevProductsContainer);
-
       productsToRemove.forEach(productEl => {
         prevProductsContainer.append(productEl);
       });
     }
 
+    if (allProducts?.length % 4 !== 0 && productsContainersElems.length > 2) {
+      const restNumber = productsElems?.length % 4;
 
-     if (allProducts?.length % 4 !== 0 && productsContainersElems.length > 2) {
-       const restNumber = productsElems?.length % 4;
-       console.log("ee", restNumber);
+      const prevProductsContainer =
+        productsContainersElems[productsContainersElems.length - 2];
+      const lastProductsContainer =
+        productsContainersElems[productsContainersElems.length - 1];
 
-       const prevProductsContainer =
-         productsContainersElems[productsContainersElems.length - 2];
-       const lastProductsContainer =
-         productsContainersElems[productsContainersElems.length - 1];
+      const products =
+        lastProductsContainerX.querySelectorAll(".product-container");
+      const productsToRemove = Array.from(products).slice(0, restNumber);
 
-       const products =
-         lastProductsContainer.querySelectorAll(".product-container");
-       const productsToRemove = Array.from(products).slice(0, restNumber);
-
-       console.log("", prevProductsContainer);
-
-       productsToRemove.forEach(productEl => {
-         prevProductsContainer.append(productEl);
-       });
-     }
+      productsToRemove.forEach(productEl => {
+        prevProductsContainer.append(productEl);
+      });
+    }
   }
 }
+
+
+// import { ModelProduct } from "../../../sharedModels/modelProduct";
+
+// export class ProductsPrinter {
+//   #products: ModelProduct[] | null = null;
+//   #productsWrapperEl = document.getElementById("productsWrapper");
+//   #productsContainerEl = document.createElement("div");
+
+//   constructor(products: ModelProduct[]) {
+//     this.#products = products;
+//     this.#createProductsContainer();
+//     this.#moveElemsToPreviousElement();
+//   }
+
+//   #createProductsElems() {
+//     this.#products?.forEach(product => {
+//       const divEl = document.createElement("div");
+//       divEl.innerText = `ID:${product.id}`;
+//       divEl.id = product.id.toString();
+//       divEl.setAttribute("data-details", JSON.stringify(product));
+//       divEl.classList.add(
+//         "product-container",
+//         "center",
+//         "cursor-pointer",
+//         "margin-auto"
+//       );
+//       this.#productsContainerEl.append(divEl);
+//     });
+//   }
+
+//   #createProductsContainer() {
+//     this.#productsContainerEl.classList.add(
+//       "even-columns-4",
+//       "relative",
+//       "products-container"
+//     );
+//     this.#productsWrapperEl?.append(this.#productsContainerEl);
+//     this.#createProductsElems();
+//   }
+
+//   #moveElemsToPreviousElement() {
+//     const productsContainersElems = document.querySelectorAll(
+//       ".products-container"
+//     );
+//     const lastProductsContainer =
+//       productsContainersElems[productsContainersElems.length - 1];
+
+//     if (lastProductsContainer) {
+//       const productsElems =
+//         lastProductsContainer.querySelectorAll(".product-container");
+
+//       if (
+//         productsElems.length % 4 !== 0 &&
+//         productsContainersElems.length > 1
+//       ) {
+//         const restNumber = productsElems.length % 4;
+//         const prevProductsContainer =
+//           productsContainersElems[productsContainersElems.length - 2];
+//         const productsToRemove = Array.from(productsElems).slice(0, restNumber);
+
+//         productsToRemove.forEach(productEl => {
+//           prevProductsContainer.append(productEl);
+//         });
+//       }
+//     }
+//   }
+// }
